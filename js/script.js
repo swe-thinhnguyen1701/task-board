71c3796ca57787b8cfb79bb9c0b15174a29a9719
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-04-28 14:47:52 -0500

    modify handleAddTask function

--- js/script.js
+++ js/script.js
@@ -1,58 +1,110 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
+if(taskList === null) taskList = [];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 const closeTaskBox = $("#close-task-box-btn");
 const showTaskBox = $("#show-task-box-btn");
 const blind = $("#blind");
 const taskBox = $("#task-box");
+const addTask = $("#add-task");
 
-closeTaskBox.on("click", function(event){
-    event.preventDefault();
-    console.log("button is clicked");
-    taskBox.hide();
-    blind.hide();
+
+closeTaskBox.on("click", function (event) {
+  taskBox.hide();
+  blind.hide();
 });
 
-showTaskBox.on("click", function(event){
-    {
-        event.preventDefault();
-        taskBox.addClass("mt-4");
-        taskBox.show();
-        blind.show();
-    }
+showTaskBox.on("click", function (event) {
+  {
+    taskBox.addClass("mt-4");
+    taskBox.show();
+    blind.show();
+  }
 });
+
+addTask.on("click", handleAddTask);
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {
-
-}
+function generateTaskId() {}
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {
-
-}
+/**
+ *
+ * @param {object} task
+ */
+function createTaskCard(task) {}
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
-
-}
+function renderTaskList() {}
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+function handleAddTask(event) {
+  event.preventDefault();
 
+  const task = {
+    title: "",
+    dat: "",
+    description: "",
+  };
+
+  const title = $("#task-title").val();
+  const date = $("#task-date").val();
+  const description = $("#task-description").val();
+  if (
+    validateData(title, "Task Title") &&
+    validateData(date, "Task Date") &&
+    validateData(description, "Task Description")
+  ) {
+    
+    task.title = title;
+    task.date = date;
+    task.description = description;
+
+    // store data
+    taskList.push(task);
+    localStorage.setItem("task", JSON.stringify(taskList));
+    console.log(taskList);
+
+    // clear form and turn off blind and task box
+    clearForm();
+    taskBox.hide();
+    blind.hide();
+  }
+
+  return;
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-
-}
+function handleDeleteTask(event) {}
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
-
-}
+function handleDrop(event, ui) {}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
+$(document).ready(function () {});
 
-});
+
+/**
+ * validateData validates data.
+ * 
+ * @param {string} data needs validating
+ * @param {string} cetegory of data
+ * @returns false if data is empty and pops up alert message.
+ *          true otherwise.
+ */
+const validateData = function (data, cetegory) {
+  if (!data || data.trim().length == 0) {
+    window.alert(`${cetegory} is required`);
+    return false;
+  }
+  return true;
+};
+
+/**
+ * clearForm erase all input data.
+ */
+const clearForm = function(){
+    $("#task-title").val("");
+    $("#task-date").val("");
+    $("#task-description").val("");
+}
